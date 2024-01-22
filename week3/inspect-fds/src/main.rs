fffb926d56b21be7db1611639712058cab45e4c9
csj <csj@jaycui.one>
2024-01-22 23:40:12 +0800

    fin w3t1m1

--- week3/inspect-fds/src/main.rs
+++ week3/inspect-fds/src/main.rs
@@ -10,11 +10,17 @@ fn main() {
         println!("Usage: {} <name or pid of target>", args[0]);
         std::process::exit(1);
     }
-    #[allow(unused)] // TODO: delete this line for Milestone 1
     let target = &args[1];
 
     // TODO: Milestone 1: Get the target Process using psutils::get_target()
-    unimplemented!();
+    let tar_process = ps_utils::get_target(target).expect("Error getting target process");
+    match tar_process {
+        Some(process) => println!("Found pid {}", process.pid),
+        None => {
+            println!("No matching process found");
+            std::process::exit(1);
+        }
+    }
 }
 
 #[cfg(test)]
