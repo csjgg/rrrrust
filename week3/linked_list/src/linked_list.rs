934b03306a3abddc07dd17a77ade59d9d884cef4
csj <csj@jaycui.one>
2024-01-24 19:39:00 +0800

    fin week3

--- week3/linked_list/src/linked_list.rs
+++ week3/linked_list/src/linked_list.rs
@@ -50,6 +50,55 @@ impl<T> LinkedList<T> {
     }
 }
 
+impl<T:Clone> Clone for LinkedList<T>{
+    fn clone(&self) -> Self {
+        let mut new_list = LinkedList::new();
+        let mut new_list2 = LinkedList::new();
+        let mut current: &Option<Box<Node<T>>> = &self.head;
+        loop{
+            match current{
+                Some(node) => {
+                    new_list2.push_front(node.value.clone());
+                    current = &node.next;
+                }
+                None => break,
+            }
+        }
+        let mut t = new_list2.pop_front();
+        loop{
+            match t{
+                Some(val) => {
+                    new_list.push_front(val.clone());
+                    t = new_list2.pop_front();
+                }
+                None => break,
+            }
+        }
+        new_list
+    }
+}
+
+impl<T:PartialEq> PartialEq for LinkedList<T>{
+    fn eq(&self, other: &Self) -> bool{
+        let mut current1: &Option<Box<Node<T>>> = &self.head;
+        let mut current2: &Option<Box<Node<T>>> = &other.head;
+        loop{
+            match (current1, current2){
+                (Some(node1), Some(node2)) => {
+                    if node1.value != node2.value{
+                        return false;
+                    }
+                    current1 = &node1.next;
+                    current2 = &node2.next;
+                }
+                (None, None) => break,
+                _ => return false,
+            }
+        }
+        true
+    }
+}
+
 impl<T: fmt::Display> fmt::Display for LinkedList<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut current: &Option<Box<Node<T>>> = &self.head;
--- week3/linked_list/src/main.rs
+++ week3/linked_list/src/main.rs
@@ -5,15 +5,24 @@ fn main() {
     let mut list: LinkedList<String> = LinkedList::new();
     assert!(list.is_empty());
     assert_eq!(list.get_size(), 0);
-    for _i in 1..12 {
-        list.push_front(String::from("hello"));
+    for i in 1..12 {
+        list.push_front(i.to_string());
     }
     println!("{}", list);
     println!("list size: {}", list.get_size());
     println!("top element: {}", list.pop_front().unwrap());
     println!("{}", list);
     println!("size: {}", list.get_size());
-    println!("{}", list.to_string()); // ToString impl for anything impl Display
+    let mut list2 = list.clone();
+    println!("list1:{}\nlist2:{}",list, list2);
+    let _ = list2.pop_front().unwrap();
+    if list == list2{
+        println!("yes");
+    }else{
+        println!("no");
+    }
+    println!("list1:{}\nlist2:{}",list, list2);
+    // println!("{}", list.to_string()); // ToString impl for anything impl Display
 
     // If you implement iterator trait:
     //for val in &list {
